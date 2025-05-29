1e8643aa5b97d2f72da089d3ae0b839ff2341898
Witold Wrześniewski <witold.wrzesniewski@poczta.fm>
2025-05-29 20:30:25 +0200

    Add Cart.initActions

--- src/js/script.js
+++ src/js/script.js
@@ -300,6 +300,7 @@
       thisCart.products = [];
 
       thisCart.getElements(element);
+      thisCart.initActions();
 
       console.log('new Cart', thisCart);
     }
@@ -308,8 +309,17 @@
       const thisCart = this;
 
       thisCart.dom = {};
-
       thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(event){
+        event.preventDefault;
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
     }
   }
 
