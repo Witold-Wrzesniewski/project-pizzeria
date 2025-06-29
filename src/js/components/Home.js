9e570589294bb8a0c5c66af1092777fffcad400e
Witold Wrześniewski <witold.wrzesniewski@poczta.fm>
2025-06-29 15:53:14 +0200

    Add home page

---
+++ src/js/components/Home.js
@@ -0,0 +1,13 @@
+class Home {
+  constructor(element){
+    const thisHome = this;
+
+    thisHome.render(element);
+  }
+  render(element){
+    const thisHome = this;
+
+    thisHome.dom = {};
+    
+  }
+}
